804110b9308ed51562df22bba359a10ebe193304
Richard Dowdle <rdowdle10@gmail.com>
2018-11-11 22:36:30 -0800

    Commit (11/11/18)

---
+++ app/src/main/java/com/ayylmao/richie/instabuy/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ayylmao.richie.instabuy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.ayylmao.richie.instabuy.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, Results.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
